1c09dc65f9652730dee38e015813c18812bb7387
Adam Fowler <adamfowler71@gmail.com>
2025-12-22 08:25:24 +0000

    Update job options to work with breaking changes in swift-jobs (#55)
    * Update job options to work with breaking changes in swift-jobs
    
    * Update package.swift, and JobOptions without breaking change

--- Sources/JobsValkey/ValkeyJobQueue.swift
+++ Sources/JobsValkey/ValkeyJobQueue.swift
@@ -64,16 +64,16 @@ public final class ValkeyJobQueue: JobQueueDriver {
     /// Options for job pushed to queue
     public struct JobOptions: JobOptionsProtocol {
         /// Delay running job until
-        public var delayUntil: Date?
+        public var delayUntil: Date
 
         /// Default initializer for JobOptions
         public init() {
-            self.delayUntil = nil
+            self.delayUntil = .now
         }
 
-        ///  Initializer for JobOptions
-        /// - Parameter delayUntil: Whether job execution should be delayed until a later date
-        public init(delayUntil: Date?) {
+        /// Initializer for JobOptions
+        /// - Parameter delayUntil: Time to delay job until
+        public init(delayUntil: Date) {
             self.delayUntil = delayUntil
         }
     }
@@ -165,7 +165,7 @@ public final class ValkeyJobQueue: JobQueueDriver {
                 self.configuration.pendingQueueKey,
                 data: [
                     .init(
-                        score: options.delayUntil?.timeIntervalSince1970 ?? Date.now.timeIntervalSince1970,
+                        score: options.delayUntil.timeIntervalSince1970,
                         member: jobID.description
                     )
                 ]
