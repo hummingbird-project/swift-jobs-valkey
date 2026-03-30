299480bc5397c594d4c8f671e05b80a694bfcc13
Adam Fowler <adamfowler71@gmail.com>
2026-03-30 08:13:30 +0100

    Store job id internally as UUID (#68)

--- Tests/JobsValkeyTests/ValkeyJobsTests.swift
+++ Tests/JobsValkeyTests/ValkeyJobsTests.swift
@@ -740,7 +740,7 @@ struct JobsValkeyTests {
 
             configuration: .init(queueName: #function, retentionPolicy: .init(cancelledJobs: .retain))
         )
-        let jobName = JobName<Int>("testCancelledJobRetention")
+        let jobName = JobName<Int>("testCancelledJobRerun")
         jobQueue.registerJob(name: jobName) { _, _ in }
 
         try await withThrowingTaskGroup(of: Void.self) { group in
@@ -799,7 +799,7 @@ struct JobsValkeyTests {
 
             configuration: .init(queueName: #function, retentionPolicy: .init(cancelledJobs: .retain))
         )
-        let jobName = JobName<Int>("testCancelledJobRetention")
+        let jobName = JobName<Int>("testCleanupProcessingJobs")
         jobQueue.registerJob(name: jobName) { _, _ in }
 
         try await withThrowingTaskGroup(of: Void.self) { group in
@@ -837,7 +837,7 @@ struct JobsValkeyTests {
         let jobQueue = try await self.createJobQueue(
             configuration: .init(queueName: #function, retentionPolicy: .init(cancelledJobs: .retain))
         )
-        let jobName = JobName<Int>("testCancelledJobRetention")
+        let jobName = JobName<Int>("testRerunProcessingJobs")
         jobQueue.registerJob(name: jobName) { _, _ in }
 
         try await withThrowingTaskGroup(of: Void.self) { group in
